6e4c7e31b53f23c70338d593ec3e8cbbf111eb2e
Gogle <it_informer@naver.com>
2017-04-02 02:56:13 +0900

    angular-material2 latest master commit apply

--- examples/cli/src/app/app.module.ts
+++ examples/cli/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import { MaterialModule, LiveAnnouncer } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -18,7 +18,7 @@ import { AppComponent } from './app.component';
     HttpModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [LiveAnnouncer],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
